c38b910a3f1f399a6f1a6af2ba013fdee473dd98
M Viraj Prabhu <virajprabhup@gmail.com>
2025-11-07 17:45:39 +0530

    fix: minor-change

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 const TITLE_TEXT = `
  ██████╗ ███████╗████████╗████████╗███████╗██████╗
@@ -14,10 +14,10 @@ const TITLE_TEXT = `
     ██║       ╚════██║   ██║   ██╔══██║██║     ██╔═██╗
     ██║       ███████║   ██║   ██║  ██║╚██████╗██║  ██╗
     ╚═╝       ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+    
  `;
 
 export default function Home() {
-
   return (
     <div className="container mx-auto max-w-3xl px-4 py-2">
       <pre className="overflow-x-auto font-mono text-sm">{TITLE_TEXT}</pre>
